69a83f97c66e678b84eba2525ecfdff01546f60f
yechan <yc.park@deepfine.ai>
2021-02-02 15:23:27 +0900

    gradle fixed

--- app/src/main/java/ai/deepfine/tiltscrollerdemo/MainActivity.kt
+++ app/src/main/java/ai/deepfine/tiltscrollerdemo/MainActivity.kt
@@ -1,11 +1,13 @@
 package ai.deepfine.tiltscrollerdemo
 
+import ai.deepfine.ycpark.OnTiltListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ai.deepfine.ycpark.TiltScroller
+import android.util.Log
 import com.ycpark.tiltscrollerdemo.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnTiltListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,12 +16,17 @@ class MainActivity : AppCompatActivity() {
 
         TiltScroller.registerSensor()
 
+        TiltScroller.onTiltListener = this
     }
 
 
     override fun onPause() {
         super.onPause()
         TiltScroller.destroy()
+    }
+
+    override fun onTilt(horizontal: Float, vertical: Float) {
+        Log.d("PYC", "horizontal : " + horizontal + "\t" + "vertical : " + vertical)
     }
 
 }
