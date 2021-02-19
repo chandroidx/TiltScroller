d181a9cd8b0fae91abb9bdd82635334b874111da
yechan <yc.park@deepfine.ai>
2021-02-19 22:06:05 +0900

    Cleanup Code

--- app/src/main/java/ai/deepfine/tiltscrollerdemo/MainActivity.kt
+++ app/src/main/java/ai/deepfine/tiltscrollerdemo/MainActivity.kt
@@ -8,13 +8,13 @@ import android.widget.TextView
 import com.ycpark.tiltscrollerdemo.R
 
 class MainActivity : AppCompatActivity(), OnTiltListener {
-    private lateinit var textView: TextView
+    private val textView: TextView by lazy {
+        findViewById<TextView>(R.id.test)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        textView = findViewById(R.id.test)
 
         TiltScroller.init(this)
 
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity(), OnTiltListener {
     }
 
     override fun onTilt(horizontal: Float, vertical: Float) {
-        textView.text = "horizontal : " + horizontal + "\n" + "vertical : " + vertical
+        textView.text = "horizontal : $horizontal\nvertical : $vertical"
     }
 
 }
